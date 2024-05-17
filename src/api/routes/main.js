5a896b952f7454ae813a61406ad8b8e232c38be5
Nannita1403 <nannimagi@gmail.com>
2024-05-17 19:32:48 +0200

    ok

--- src/api/routes/main.js
+++ src/api/routes/main.js
@@ -1,13 +1,13 @@
 // Nos traemos todas las rutas de index.js
 
-const cantanteRouter = require("./cantantes");
 const usersRoutes = require("./users");
 const songsRouter = require("./songs");
+const singerRouter = require("./singers");
 
 const mainRouter = require ("express").Router();
 
 mainRouter.use("/users", usersRoutes);
-mainRouter.use("/cantantes", cantanteRouter);
+mainRouter.use("/singers", singerRouter);
 mainRouter.use("/songs", songsRouter);
 
 
