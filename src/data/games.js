5010d093f0a6d4c021ec2da3c6801d2a8a27208e
Nannita1403 <nannimagi@gmail.com>
2024-05-30 15:28:41 +0200

    .

---
+++ src/data/games.js
@@ -0,0 +1,20 @@
+
+const games = [
+    {
+        nombre: "titulo",
+        imagen: "url de la imagen",
+        price: 116,
+        category: ["categoría", "otra categoría"]
+    },
+    {
+        nombre: "titulo",
+        imagen: "url de la imagen",
+        price: 116,
+        category: ["categoría", "otra categoría"]
+    }
+    // meter todos los datos que queráis
+ ];
+ 
+ module.exports = games
+ 
+ 
